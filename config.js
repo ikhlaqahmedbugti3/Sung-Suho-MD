493e764efeefbe86967c2e59be88619700621bef
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2026-01-01 11:58:19 +0500

    Update SESSION_ID and OWNER_NUM in config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
-  OWNER_NUM: process.env.OWNER_NUM || "27649342626",
+  SESSION_ID: process.env.SESSION_ID || "PE9jTZzY#DbtPAkkTeVSpN_wc5JBmje6uhJhD58Df8v7IkpTc1pY",
+  OWNER_NUM: process.env.OWNER_NUM || "923334104708",
   OWNER_NAME: process.env.OWNER_NAME || "Lord Sung",
   REPO: process.env.REPO || "https://github.com/NaCkS-ai/Sung-Suho-MD", 
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
-  MODE : process.env.MODE || "public", 
+  MODE : process.env.MODE || "private", 
   AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
   AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "false"), 
   ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
